6485937d3f17a55b03d3a120af58fc2542170838
GR <grvig06@gmail.com>
2026-06-13 14:33:36 +0530

    Added budget viewmodel factory

---
+++ app/src/main/java/com/grvig/financetracker/viewmodel/BudgetViewModelFactory.kt
@@ -0,0 +1,30 @@
+package com.grvig.financetracker.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.grvig.financetracker.repository.BudgetRepository
+
+class BudgetViewModelFactory(
+    private val repository: BudgetRepository
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(
+        modelClass: Class<T>
+    ): T {
+
+        if (
+            modelClass.isAssignableFrom(
+                BudgetViewModel::class.java
+            )
+        ) {
+            @Suppress("UNCHECKED_CAST")
+            return BudgetViewModel(
+                repository
+            ) as T
+        }
+
+        throw IllegalArgumentException(
+            "Unknown ViewModel class"
+        )
+    }
+}
