e01d8d39aa2c5bd24c5da0d6b2d3281b64c52aa8
GR <grvig06@gmail.com>
2026-08-07 19:36:19 +0530

    Bumped version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.grvig.financetracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
